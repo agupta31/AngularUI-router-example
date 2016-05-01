6808388bb511d33b9aca63b0226d311e1c0b00c0
agupta31 <akash2989@gmail.com>
2016-04-30 22:11:39 -0400

    added ui router

---
+++ AngularUIRouter/app.js
@@ -0,0 +1,70 @@
+var myApp=angular.module("testApp",["ui.router"]);
+
+myApp.config(function($stateProvider,$urlRouterProvider){
+     
+      //setting up the states
+    
+    $stateProvider
+       
+     .state("state1",{
+           url:"/state1",
+          templateUrl:"partials/state1.html"
+        
+    })
+    
+     .state("state1.list",{
+        
+            url:"/state1.list",
+            templateUrl:"partials/state1.list.html",
+            controller:"appCtrl"
+    })
+    
+    
+    
+     .state("state2",{
+           url:"/state2",
+           templateUrl:"partials/state2.html"
+    })
+    
+    .state("state2.list",{
+          url:"/state2.list",
+          templateUrl:"partials/state2.list.html",
+          controller:"appCtrl"
+    })
+    
+   
+});
+
+
+
+
+myApp.controller("appCtrl",function($scope){
+    
+       $scope.person=[
+           
+                       {
+                           name:'akash',
+                           age:26
+                       },
+                      {    name:'utpal',
+                           age:60
+               
+                       }
+                   ];
+    
+    
+      $scope.foods=[
+          
+                        {
+                            name:"biryani",
+                            points:100
+                        } ,
+                         {
+                             name:"kebab",
+                             points:95
+                         }
+          
+                
+      ];
+      
+});
